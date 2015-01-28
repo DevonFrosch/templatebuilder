91f4ef9e217a915c4394a0cf326dc0d2947095d3
DevonFrosch <bitbucket@devonfrosch.eu>
2015-01-28 02:42:59 +0100

    Strecke: anzahlGleise besser versteckt

--- src/de/stsFanGruppe/strecken/Strecke.java
+++ src/de/stsFanGruppe/strecken/Strecke.java
@@ -20,16 +20,7 @@ public class Strecke
 	protected Streckengleis streckengleis0;
 	protected Streckengleis streckengleis1;
 	
-	/**
-	 * Erzeugt eine zweigleisige Strecke
-	 * @param name Name der Strecke
-	 * @param anfang
-	 * @param ende
-	 * @param anzahlGleise
-	 * @param streckengleis0
-	 * @param streckengleis1
-	 */
-	public Strecke(String name, Betriebsstelle anfang, Betriebsstelle ende, int anzahlGleise,
+	protected Strecke(String name, Betriebsstelle anfang, Betriebsstelle ende, int anzahlGleise,
 			Streckengleis streckengleis0, Streckengleis streckengleis1)
 	{
 		this.name = name;
@@ -40,17 +31,29 @@ public class Strecke
 		this.streckengleis1 = streckengleis1;
 	}
 	/**
+	 * Erzeugt eine zweigleisige Strecke
+	 * @param name Name der Strecke
+	 * @param anfang
+	 * @param ende
+	 * @param streckengleis0
+	 * @param streckengleis1
+	 */
+	public Strecke(String name, Betriebsstelle anfang, Betriebsstelle ende,
+			Streckengleis streckengleis0, Streckengleis streckengleis1)
+	{
+		this(name, anfang, ende, 2, streckengleis0, streckengleis1);
+	}
+	/**
 	 * Erzeugt eine eingleisige Strecke
 	 * @param name
 	 * @param anfang
 	 * @param ende
-	 * @param anzahlGleise
 	 * @param streckengleis
 	 */
-	public Strecke(String name, Betriebsstelle anfang, Betriebsstelle ende, int anzahlGleise,
+	public Strecke(String name, Betriebsstelle anfang, Betriebsstelle ende,
 			Streckengleis streckengleis)
 	{
-		this(name, anfang, ende, anzahlGleise, streckengleis, null);
+		this(name, anfang, ende, 1, streckengleis, null);
 	}
 	
 	public String getName()
@@ -81,10 +84,6 @@ public class Strecke
 	{
 		return anzahlGleise;
 	}
-	public void setAnzahlGleise(int anzahlGleise)
-	{
-		this.anzahlGleise = anzahlGleise;
-	}
 	public Streckengleis getStreckengleis0()
 	{
 		return streckengleis0;
@@ -95,10 +94,18 @@ public class Strecke
 	}
 	public Streckengleis getStreckengleis1()
 	{
-		return streckengleis1;
+		if(anzahlGleise > 1)
+			return streckengleis1;
+		return null;
 	}
 	public void setStreckengleis1(Streckengleis streckengleis1)
 	{
 		this.streckengleis1 = streckengleis1;
+		this.anzahlGleise = 2;
+	}
+	public void removeStreckengleis1()
+	{
+		this.streckengleis1 = null;
+		this.anzahlGleise = 1;
 	}
 }
