af06db9356efebfb443cb04da57f088fba5fd6e7
DevonFrosch <bitbucket@devonfrosch.eu>
2015-11-08 01:11:07 +0100

    Fahrplanhalt: Eigenschaften in FahrplanhaltEigenschaften ausgelagert

--- src/de/stsFanGruppe/templatebuilder/zug/Fahrplanhalt.java
+++ src/de/stsFanGruppe/templatebuilder/zug/Fahrplanhalt.java
@@ -1,7 +1,5 @@
 package de.stsFanGruppe.templatebuilder.zug;
 
-import java.util.LinkedList;
-import java.util.List;
 import de.stsFanGruppe.templatebuilder.strecken.Gleisabschnitt;
 
 /**
@@ -15,9 +13,9 @@ public class Fahrplanhalt implements Comparable<Fahrplanhalt>
 	protected Gleisabschnitt gleisabschnitt;
 	protected double ankunft;
 	protected double abfahrt;
-	protected Eigenschaften eigenschaften;
+	protected FahrplanhaltEigenschaften eigenschaften;
 	
-	public Fahrplanhalt(Gleisabschnitt gleisabschnitt, double ankunft, double abfahrt, Eigenschaften eigenschaften)
+	public Fahrplanhalt(Gleisabschnitt gleisabschnitt, double ankunft, double abfahrt, FahrplanhaltEigenschaften eigenschaften)
 	{
 		this.gleisabschnitt = gleisabschnitt;
 		this.ankunft = ankunft;
@@ -49,11 +47,11 @@ public class Fahrplanhalt implements Comparable<Fahrplanhalt>
 	{
 		this.abfahrt = abfahrt;
 	}
-	public Eigenschaften getEigenschaften()
+	public FahrplanhaltEigenschaften getEigenschaften()
 	{
 		return eigenschaften;
 	}
-	public void setEigenschaften(Eigenschaften eigenschaften)
+	public void setEigenschaften(FahrplanhaltEigenschaften eigenschaften)
 	{
 		this.eigenschaften = eigenschaften;
 	}
@@ -98,126 +96,5 @@ public class Fahrplanhalt implements Comparable<Fahrplanhalt>
 		}
 		// ... dann nach Abfahrt
 		return Double.compare(this.abfahrt, other.abfahrt);
-	}
-	
-	public class Eigenschaften
-	{
-		// TODO Kuppeln und Flügeln implementieren
-		protected boolean hasDurchfahrt;
-		protected boolean hasRichtungswechsel;
-		protected boolean hasNamenswechsel;
-		protected Fahrt namenswechsel;
-
-		public Eigenschaften(boolean hasDurchfahrt, boolean hasRichtungswechsel, Fahrt namenswechsel)
-		{
-			this.hasDurchfahrt = hasDurchfahrt;
-			this.hasRichtungswechsel = hasRichtungswechsel;
-			this.namenswechsel = namenswechsel;
-			if(namenswechsel != null)
-				this.hasNamenswechsel = true;
-		}
-		
-		public boolean hasDurchfahrt()
-		{
-			return hasDurchfahrt;
-		}
-		public void setDurchfahrt(boolean hasDurchfahrt)
-		{
-			this.hasDurchfahrt = hasDurchfahrt;
-		}
-		public boolean hasRichtungswechsel()
-		{
-			return hasRichtungswechsel;
-		}
-		public void setRichtungswechsel(boolean hasRichtungswechsel)
-		{
-			this.hasRichtungswechsel = hasRichtungswechsel;
-		}
-		public boolean hasNamenswechsel()
-		{
-			return hasNamenswechsel;
-		}
-		/**
-		 * Gibt den Nachfolger zurück. Ist kein Namenswechsel aktiv, oder ist kein Nachfolger gesetzt, wird null zurück gegeben.
-		 * @return Die Fahrt, auf die der Name geändert wird, oder null
-		 */
-		public Fahrt getNamenswechselNachfolger()
-		{
-			if(hasNamenswechsel)
-				return namenswechsel;
-			else
-				return null;
-		}
-		public void setNamenswechselNachfolger(Fahrt namenswechsel)
-		{
-			this.hasNamenswechsel = true;
-			this.namenswechsel = namenswechsel;
-		}
-		public void removeNamenswechsel()
-		{
-			this.namenswechsel = null;
-			this.hasNamenswechsel = false;
-		}
-		
-		public String toString()
-		{
-			StringBuilder str = new StringBuilder("Fahrplanhalt-Eigenschaften { ");
-			
-			List<String> opts = new LinkedList<>();
-			if(hasDurchfahrt)
-			{
-				opts.add("fährt durch");
-			}
-			if(hasRichtungswechsel)
-			{
-				opts.add("ändert Richtung");
-			}
-			if(hasNamenswechsel)
-			{
-				if(namenswechsel != null)
-				{
-					opts.add("ändert Name zu "+namenswechsel.getName());
-				}
-				else
-				{
-					opts.add("ändert Name (kein Ziel)");
-				}
-			}
-			str.append(String.join(", ", (String[]) opts.toArray()));
-			
-			str.append(" }");
-			return str.toString();
-		}
-		public String toXML()
-		{
-			return toXML("");
-		}
-		public String toXML(String indent)
-		{
-			StringBuilder str = new StringBuilder(indent+"<fahrplanhalt");
-			
-			if(hasDurchfahrt)
-			{
-				str.append(" durchfahrt=\"true\"");
-			}
-			if(hasRichtungswechsel)
-			{
-				str.append(" richtungswechsel=\"true\"");
-			}
-			if(hasNamenswechsel)
-			{
-				if(namenswechsel != null)
-				{
-					str.append(" namenswechsel=\""+namenswechsel.getName()+"\"");
-				}
-				else
-				{
-					str.append(" namenswechsel=\"null\"");
-				}
-			}
-			
-			str.append(" />\n");
-			return str.toString();
-		}
 	}
 }
---
+++ src/de/stsFanGruppe/templatebuilder/zug/FahrplanhaltEigenschaften.java
@@ -0,0 +1,125 @@
+package de.stsFanGruppe.templatebuilder.zug;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class FahrplanhaltEigenschaften
+{
+	// TODO Kuppeln und Flügeln implementieren
+	protected boolean hasDurchfahrt;
+	protected boolean hasRichtungswechsel;
+	protected boolean hasNamenswechsel;
+	protected Fahrt namenswechsel;
+
+	public FahrplanhaltEigenschaften(boolean hasDurchfahrt, boolean hasRichtungswechsel, Fahrt namenswechsel)
+	{
+		this.hasDurchfahrt = hasDurchfahrt;
+		this.hasRichtungswechsel = hasRichtungswechsel;
+		this.namenswechsel = namenswechsel;
+		if(namenswechsel != null)
+			this.hasNamenswechsel = true;
+	}
+	
+	public boolean hasDurchfahrt()
+	{
+		return hasDurchfahrt;
+	}
+	public void setDurchfahrt(boolean hasDurchfahrt)
+	{
+		this.hasDurchfahrt = hasDurchfahrt;
+	}
+	public boolean hasRichtungswechsel()
+	{
+		return hasRichtungswechsel;
+	}
+	public void setRichtungswechsel(boolean hasRichtungswechsel)
+	{
+		this.hasRichtungswechsel = hasRichtungswechsel;
+	}
+	public boolean hasNamenswechsel()
+	{
+		return hasNamenswechsel;
+	}
+	/**
+	 * Gibt den Nachfolger zurück. Ist kein Namenswechsel aktiv, oder ist kein Nachfolger gesetzt, wird null zurück gegeben.
+	 * @return Die Fahrt, auf die der Name geändert wird, oder null
+	 */
+	public Fahrt getNamenswechselNachfolger()
+	{
+		if(hasNamenswechsel)
+			return namenswechsel;
+		else
+			return null;
+	}
+	public void setNamenswechselNachfolger(Fahrt namenswechsel)
+	{
+		this.hasNamenswechsel = true;
+		this.namenswechsel = namenswechsel;
+	}
+	public void removeNamenswechsel()
+	{
+		this.namenswechsel = null;
+		this.hasNamenswechsel = false;
+	}
+	
+	public String toString()
+	{
+		StringBuilder str = new StringBuilder("Fahrplanhalt-Eigenschaften { ");
+		
+		List<String> opts = new LinkedList<>();
+		if(hasDurchfahrt)
+		{
+			opts.add("fährt durch");
+		}
+		if(hasRichtungswechsel)
+		{
+			opts.add("ändert Richtung");
+		}
+		if(hasNamenswechsel)
+		{
+			if(namenswechsel != null)
+			{
+				opts.add("ändert Name zu "+namenswechsel.getName());
+			}
+			else
+			{
+				opts.add("ändert Name (kein Ziel)");
+			}
+		}
+		str.append(String.join(", ", (String[]) opts.toArray()));
+		
+		str.append(" }");
+		return str.toString();
+	}
+	public String toXML()
+	{
+		return toXML("");
+	}
+	public String toXML(String indent)
+	{
+		StringBuilder str = new StringBuilder(indent+"<fahrplanhalt");
+		
+		if(hasDurchfahrt)
+		{
+			str.append(" durchfahrt=\"true\"");
+		}
+		if(hasRichtungswechsel)
+		{
+			str.append(" richtungswechsel=\"true\"");
+		}
+		if(hasNamenswechsel)
+		{
+			if(namenswechsel != null)
+			{
+				str.append(" namenswechsel=\""+namenswechsel.getName()+"\"");
+			}
+			else
+			{
+				str.append(" namenswechsel=\"null\"");
+			}
+		}
+		
+		str.append(" />\n");
+		return str.toString();
+	}
+}
