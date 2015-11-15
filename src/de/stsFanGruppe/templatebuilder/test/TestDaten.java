43eebc5fe4585136205dc5c1e8f628a16dc82cb7
DevonFrosch <bitbucket@devonfrosch.eu>
2015-11-15 17:29:37 +0100

    TestDaten: Strecken-Tests weg

--- src/de/stsFanGruppe/templatebuilder/test/TestDaten.java
+++ src/de/stsFanGruppe/templatebuilder/test/TestDaten.java
@@ -5,16 +5,6 @@ import de.stsFanGruppe.templatebuilder.strecken.*;
 
 public class TestDaten
 {
-	// Paket Strecke
-	public static Gleis gleis()
-	{
-		return new Gleis("Testgleis 1");
-	}
-	public static FahrplanhaltEigenschaften eigenschaften()
-	{
-		return new FahrplanhaltEigenschaften(false, false, null);
-	}
-	
 	// Paket Zug
 	public static Fahrt fahrt()
 	{
@@ -22,9 +12,9 @@ public class TestDaten
 		Gleis g1 = new Gleis("A-Stadt");
 		Gleis g2 = new Gleis("B-Hausen");
 		Gleis g3 = new Gleis("C-Dorf");
-		f.addFahrplanhalt(new Fahrplanhalt(g1.getGleisabschnitte().first(), 0, 0, eigenschaften()));
-		f.addFahrplanhalt(new Fahrplanhalt(g2.getGleisabschnitte().first(), 10, 15, eigenschaften()));
-		f.addFahrplanhalt(new Fahrplanhalt(g3.getGleisabschnitte().first(), 25, 25, eigenschaften()));
+		f.addFahrplanhalt(new Fahrplanhalt(g1.getGleisabschnitte().first(), 0, 0, new FahrplanhaltEigenschaften()));
+		f.addFahrplanhalt(new Fahrplanhalt(g2.getGleisabschnitte().first(), 10, 15, new FahrplanhaltEigenschaften()));
+		f.addFahrplanhalt(new Fahrplanhalt(g3.getGleisabschnitte().first(), 25, 25, new FahrplanhaltEigenschaften()));
 		return f;
 	}
 }
