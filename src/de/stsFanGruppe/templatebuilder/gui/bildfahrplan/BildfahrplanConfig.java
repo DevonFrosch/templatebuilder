57c504f42b1291764b1e2d0257958386534e8604
DevonFrosch <bitbucket@devonfrosch.eu>
2015-11-20 22:03:59 +0100

    BildfahrplanConfig: XML-Import

--- src/de/stsFanGruppe/templatebuilder/gui/bildfahrplan/BildfahrplanConfig.java
+++ src/de/stsFanGruppe/templatebuilder/gui/bildfahrplan/BildfahrplanConfig.java
@@ -1,6 +1,11 @@
 package de.stsFanGruppe.templatebuilder.gui.bildfahrplan;
 
+import java.io.FileNotFoundException;
 import javax.swing.JPanel;
+import javax.xml.stream.XMLStreamException;
+import de.stsFanGruppe.templatebuilder.external.ImportException;
+import de.stsFanGruppe.templatebuilder.external.xmlhelper.XMLElement;
+import de.stsFanGruppe.templatebuilder.external.xmlhelper.XMLReader;
 
 public class BildfahrplanConfig
 {
@@ -17,6 +22,10 @@ public class BildfahrplanConfig
 	{
 		this.setMinZeit(minZeit);
 		this.setMaxZeit(maxZeit);
+	}
+	public BildfahrplanConfig(String xml) throws ImportException
+	{
+		this.readFromXML(xml);
 	}
 	
 	public double getMinZeit()
@@ -74,6 +83,32 @@ public class BildfahrplanConfig
 				+newLine+"<hoeheProStunde value=\""+hoeheProStunde+"\" />"
 				+newLine+"<zeitBereich min="+minZeit+" max=\""+maxZeit+"\" />"
 				+"\n"+indent+"</bildfahrplanConfig>";
+	}
+	public void readFromXML(String xml) throws ImportException
+	{
+		try
+		{
+			XMLReader reader = new XMLReader(new java.io.FileInputStream(xml));
+			XMLElement elem;
+			while((elem = reader.findTagUntil("bildfahrplanConfig", "hoeheProStunde", "zeitBereich")) != null)
+			{
+				switch(elem.getName().toLowerCase())
+				{
+					case "hoeheprostunde":
+						this.hoeheProStunde = Integer.parseInt(elem.getAttribute("value"));
+						break;
+					case "zeitbereich":
+						this.minZeit = Double.parseDouble(elem.getAttribute("min"));
+						this.maxZeit = Double.parseDouble(elem.getAttribute("max"));
+					default:
+						break;
+				}
+			}
+		}
+		catch(FileNotFoundException | XMLStreamException | NumberFormatException e)
+		{
+			throw new ImportException(e);
+		}
 	}
 	
 	private static void log(String text)
