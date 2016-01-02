ffeba5b94445ee402172fb8a2992150d56b1f692
kkscript <kai.kosche@mail.de>
2016-01-02 21:43:41 +0100

    BT074: GUI erstellt - Inhalt für die GUI fehlt noch

--- src/de/stsFanGruppe/templatebuilder/fahrtenFarbe/FahrtenFarbeSettingsGUI.java
+++ src/de/stsFanGruppe/templatebuilder/fahrtenFarbe/FahrtenFarbeSettingsGUI.java
@@ -1,5 +1,42 @@
 package de.stsFanGruppe.templatebuilder.fahrtenFarbe;
 
-public class FahrtenFarbeSettingsGUI {
+import java.awt.Window;
 
+import javax.swing.JPanel;
+
+import de.stsFanGruppe.templatebuilder.config.BildfahrplanConfig;
+import de.stsFanGruppe.templatebuilder.config.BildfahrplanSettingsGUI;
+import de.stsFanGruppe.templatebuilder.config.BildfahrplanSettingsGUIController;
+import de.stsFanGruppe.tools.NullTester;
+
+public class FahrtenFarbeSettingsGUI 
+{
+	protected static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(BildfahrplanSettingsGUI.class);
+	
+	FahrtenFarbeSettingsGUIController controller;
+	FahrtenFarbeConfig config;
+	boolean saveEnabled = false;
+	
+	final JPanel contentPanel = new JPanel();
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			FahrtenFarbeSettingsGUI dialog = new FahrtenFarbeSettingsGUI(new FahrtenFarbeSettingsGUIController(new FahrtenFarbeConfig(), () -> {}), null);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public FahrtenFarbeSettingsGUI(FahrtenFarbeSettingsGUIController controller, Window parent) 
+	{
+		super(parent);
+		NullTester.test(controller);
+		this.controller = controller;
+		this.config = controller.getConfig();
+		controller.setSettingsGui(this);
+	}
 }
--- src/de/stsFanGruppe/templatebuilder/fahrtenFarbe/FahrtenFarbeSettingsGUIController.java
+++ src/de/stsFanGruppe/templatebuilder/fahrtenFarbe/FahrtenFarbeSettingsGUIController.java
@@ -1,5 +1,35 @@
 package de.stsFanGruppe.templatebuilder.fahrtenFarbe;
 
-public class FahrtenFarbeSettingsGUIController {
+import de.stsFanGruppe.templatebuilder.config.BildfahrplanSettingsGUIController;
+import de.stsFanGruppe.tools.NullTester;
 
+public class FahrtenFarbeSettingsGUIController {
+	
+	protected static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(BildfahrplanSettingsGUIController.class);
+	
+	private FahrtenFarbeSettingsGUI gui;
+	private FahrtenFarbeConfig config;
+	private Runnable onClose;
+
+	public FahrtenFarbeSettingsGUIController(FahrtenFarbeConfig config, Runnable onClose)
+	{
+		NullTester.test(config);
+		NullTester.test(onClose);
+		this.config = config;
+		this.onClose = onClose;
+	}
+
+	public void setSettingsGui(FahrtenFarbeSettingsGUI gui)
+	{
+		NullTester.test(gui);
+		this.gui = gui;
+		gui.saveEnabled = config.schreibTest();
+	}
+	
+	public FahrtenFarbeConfig getConfig()
+	{
+		return config;
+	}
+
+	
 }
