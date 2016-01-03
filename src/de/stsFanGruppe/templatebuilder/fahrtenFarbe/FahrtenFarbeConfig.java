57118c4af2b66a152f67262caaeef74b7e38a88d
kkscript <kai.kosche@mail.de>
2016-01-03 15:42:56 +0100

    BT074: Standardeinstellung in Config eingebaut

--- src/de/stsFanGruppe/templatebuilder/fahrtenFarbe/FahrtenFarbeConfig.java
+++ src/de/stsFanGruppe/templatebuilder/fahrtenFarbe/FahrtenFarbeConfig.java
@@ -1,7 +1,18 @@
 package de.stsFanGruppe.templatebuilder.fahrtenFarbe;
 
+import java.awt.Color;
+
 public class FahrtenFarbeConfig {
 
+	//Farbeinstellungen
+	public static final String CONFIG_LINIEN_FARBE = "bildfahrplan/darstellung/farben/zeiten";
+	public static final String CONFIG_LINIEN_STAERKE  = "bildfahrplan/darstellung/farben/fahrten";
+	public static final String CONFIG_LINIEN_ART = "bildfahrplan/darstellung/farben/betriebsstellen";
+	
+	private static final Color DEFAULT_LINIEN_FARBE = Color.RED;
+	private static final int DEFAULT_LINIEN_STAERKE = 1;
+	private static final Color DEFAULT_LINIEN_ART = Color.BLUE;
+	
 	public void exportSettings(FahrtenFarbeSettingsGUI gui, FahrtenFarbeSettingsGUI gui2) {
 		// TODO Auto-generated method stub
 		
