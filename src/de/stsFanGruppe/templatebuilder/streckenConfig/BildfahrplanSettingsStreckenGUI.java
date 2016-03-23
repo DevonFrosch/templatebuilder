75c5bd1464581eb89ea7cb4800e76feda9285d0c
kkscript <kai.kosche@mail.de>
2016-03-23 01:10:15 +0100

    BT013: 4 Klassen für die StreckenbearbeitungsGUI erstellt

---
+++ src/de/stsFanGruppe/templatebuilder/streckenConfig/BildfahrplanSettingsStreckenGUI.java
@@ -0,0 +1,165 @@
+package de.stsFanGruppe.templatebuilder.streckenConfig;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+
+import de.stsFanGruppe.templatebuilder.fahrtenFarbe.FahrtenFarbeConfig;
+import de.stsFanGruppe.templatebuilder.fahrtenFarbe.FahrtenFarbeSettingsGUIController;
+import de.stsFanGruppe.templatebuilder.gui.GUI;
+import de.stsFanGruppe.tools.NullTester;
+import de.stsFanGruppe.tools.TableModel;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.table.DefaultTableModel;
+
+import com.jgoodies.forms.layout.FormSpecs;
+
+public class BildfahrplanSettingsStreckenGUI extends JDialog implements GUI
+{
+	protected static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(BildfahrplanSettingsStreckenGUI.class);
+	
+	BildfahrplanSettingsStreckenGUIController controller;
+	BildfahrplanStreckenConfig config;
+	boolean saveEnabled = false;
+	
+	JLabel lblDescription;
+	JTable table;
+	
+	public final String[] columnName = {"km", "Bestriebsstelle"};
+	private JTextField textField;
+	
+	final JPanel contentPanel = new JPanel();
+		
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args)
+	{
+		try
+		{
+			BildfahrplanSettingsStreckenGUI dialog = new BildfahrplanSettingsStreckenGUI(new BildfahrplanSettingsStreckenGUIController(new BildfahrplanStreckenConfig(), () -> {}), null);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Create the dialog.
+	 */
+	public BildfahrplanSettingsStreckenGUI(BildfahrplanSettingsStreckenGUIController controller, Window parent)
+	{
+		super(parent);
+		NullTester.test(controller);
+		this.controller = controller;
+		this.config = controller.getConfig();
+		controller.setSettingsGUI(this);
+		
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		setBounds(0, 0, 500, 500);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		{
+			FormLayout fl_panel = new FormLayout(
+					new ColumnSpec[] {FormSpecs.RELATED_GAP_COLSPEC, ColumnSpec.decode("default:grow"), FormSpecs.RELATED_GAP_COLSPEC,
+							FormSpecs.DEFAULT_COLSPEC, FormSpecs.LABEL_COMPONENT_GAP_COLSPEC,},
+					new RowSpec[] {FormSpecs.RELATED_GAP_ROWSPEC, RowSpec.decode("default:grow"), FormSpecs.RELATED_GAP_ROWSPEC,
+							FormSpecs.DEFAULT_ROWSPEC, FormSpecs.UNRELATED_GAP_ROWSPEC, RowSpec.decode("default:grow"),
+							FormSpecs.RELATED_GAP_ROWSPEC,});
+			contentPanel.setLayout(fl_panel);
+			{
+				JLabel lblDiscription = new JLabel();
+				contentPanel.add(lblDiscription, "2, 2, 3, 1");
+				lblDiscription
+						.setText("<html>Hier kannst du die Strecke bearbeiten.</html>");
+			}
+			{
+				// TODO Nach Testphase entfernen
+				Object[][] data = {{new Integer(5), "Hamburg"}, {new Integer(10), "John"}};
+						
+				DefaultTableModel tableModel = new DefaultTableModel();
+				tableModel.setDataVector(data, columnName);
+				
+				table = new JTable(tableModel);
+				table.getTableHeader().setReorderingAllowed(false);
+				
+				JScrollPane scrollPane = new JScrollPane(table);
+				scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+				scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+				contentPanel.add(scrollPane, "2, 4");
+			}
+			{
+				JPanel buttonTablePane = new JPanel();
+				contentPanel.add(buttonTablePane, "4, 4, default, top");
+				buttonTablePane.setLayout(new FormLayout(new ColumnSpec[] {FormSpecs.DEFAULT_COLSPEC,},
+						new RowSpec[] {FormSpecs.LABEL_COMPONENT_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, FormSpecs.LABEL_COMPONENT_GAP_ROWSPEC,
+								FormSpecs.DEFAULT_ROWSPEC, FormSpecs.LABEL_COMPONENT_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC,
+								FormSpecs.LABEL_COMPONENT_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, FormSpecs.LABEL_COMPONENT_GAP_ROWSPEC,}));
+				{
+					JButton btnMoveUp = new JButton("Oben");
+					btnMoveUp.setActionCommand("moveUpRow");
+					btnMoveUp.addActionListener((ActionEvent arg0) -> controller.tableButtonAction(arg0));
+					buttonTablePane.add(btnMoveUp, "1, 2");
+				}
+				{
+					JButton btnMoveDown = new JButton("Unten");
+					btnMoveDown.setActionCommand("moveDownRow");
+					btnMoveDown.addActionListener((ActionEvent arg0) -> controller.tableButtonAction(arg0));
+					buttonTablePane.add(btnMoveDown, "1, 4");
+				}
+				{
+					JButton btnAddRow = new JButton("+");
+					btnAddRow.setActionCommand("addRow");
+					btnAddRow.addActionListener((ActionEvent arg0) -> controller.tableButtonAction(arg0));
+					buttonTablePane.add(btnAddRow, "1, 6");
+				}
+				{
+					JButton btnRemoveRow = new JButton("-");
+					btnRemoveRow.setActionCommand("removeRow");
+					btnRemoveRow.addActionListener((ActionEvent arg0) -> controller.tableButtonAction(arg0));
+					buttonTablePane.add(btnRemoveRow, "1, 8");
+				}
+			}
+		}
+		loadSettings();
+		setVisible(true);
+	}
+	
+	public void close()
+	{
+		dispose();
+        setVisible(false);
+        controller = null;
+	}
+	
+	public void errorMessage(String text, String titel)
+	{
+		JOptionPane.showMessageDialog(contentPanel, text, titel, JOptionPane.ERROR_MESSAGE);
+	}
+	public void warningMessage(String text, String titel)
+	{
+		JOptionPane.showMessageDialog(contentPanel, text, titel, JOptionPane.WARNING_MESSAGE);
+	}
+	public void infoMessage(String text, String titel)
+	{
+		JOptionPane.showMessageDialog(contentPanel, text, titel, JOptionPane.INFORMATION_MESSAGE);
+	}
+	
+	/**
+	 * Beim Öffnen der GUI werden die Strecken für die Tabelle ausgelesen und korrekt dargestellt.
+	 */
+	public void loadSettings() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/de/stsFanGruppe/templatebuilder/streckenConfig/BildfahrplanSettingsStreckenGUIController.java
@@ -0,0 +1,244 @@
+package de.stsFanGruppe.templatebuilder.streckenConfig;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import javax.swing.JColorChooser;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.DefaultTableModel;
+
+import de.stsFanGruppe.tools.NullTester;
+import de.stsFanGruppe.tools.TimeFormater;
+
+public class BildfahrplanSettingsStreckenGUIController
+{
+	protected static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(BildfahrplanSettingsStreckenGUIController.class);
+	
+	private BildfahrplanSettingsStreckenGUI gui;
+	private BildfahrplanStreckenConfig config;
+	private Runnable onClose;
+	
+	public BildfahrplanSettingsStreckenGUIController(BildfahrplanStreckenConfig config, Runnable onClose)
+	{
+		NullTester.test(config);
+		NullTester.test(onClose);
+		this.config = config;
+		this.onClose = onClose;
+	}
+	
+	public void setSettingsGUI(BildfahrplanSettingsStreckenGUI gui)
+	{
+		NullTester.test(gui);
+		this.gui = gui;
+		gui.saveEnabled = config.schreibTest();
+	}
+	
+	public BildfahrplanStreckenConfig getConfig()
+	{
+		return config;
+	}
+	
+	public String getMinZeit()
+	{
+		return TimeFormater.doubleToString(config.getMinZeit());
+	}
+	public String getMaxZeit()
+	{
+		return TimeFormater.doubleToString(config.getMaxZeit());
+	}
+	
+	public void farbButton(ActionEvent event)
+	{
+		assert config != null;
+		
+		// Ohne GUI können wir nichts machen
+		if(gui == null)
+		{
+			log.error("farbButton(): Keine GUI gesetzt!");
+			return;
+		}
+		
+		Color c = null;
+		switch(event.getActionCommand())
+		{
+//			s
+		}
+	}
+	/**
+	 * Liest den ActionCommand vom event aus und führt dann etwas aus.
+	 * @param event
+	 */
+	public void tableButtonAction(ActionEvent event)
+	{
+		assert config != null;
+		
+		// Ohne GUI können wir nichts machen
+		if(gui == null)
+		{
+			log.error("actionButton(): Keine GUI gesetzt!");
+			return;
+		}
+		//Tabellenmodell wird ausgelesen
+		DefaultTableModel model = (DefaultTableModel)gui.table.getModel();
+		//Selectionmodell wird ausgelesen
+		ListSelectionModel selectionModel = gui.table.getSelectionModel();
+		//Erhalte ausgewählte Zeilen
+		int[] rows = gui.table.getSelectedRows();
+		Object[] defaultRowdata = {"", ""};
+		switch(event.getActionCommand())
+		{
+			//Zeile(n) nach oben verschieben
+			case "moveUpRow":
+				if(rows[0]==0)
+				{
+					gui.infoMessage("Erste Zeile kann nicht nach oben verschoben werden.", "Info: Zeile nach oben verschieben");
+				}
+				else
+				{
+					selectionModel.clearSelection();
+					for(int i=0; i < rows.length; i++)
+					{
+						model.moveRow(rows[i], rows[i], rows[i]-1);
+						gui.table.addRowSelectionInterval(rows[i]-1, rows[i]-1);
+					}
+				}
+				break;
+			//Zeile(n) nach unten verschieben
+			case "moveDownRow":
+				selectionModel.clearSelection();
+				for(int i=0; i < rows.length; i++)
+				{
+					model.moveRow(rows[i], rows[i], rows[i]+1);
+					gui.table.addRowSelectionInterval(rows[i]+1, rows[i]+1);
+				}
+				break;
+			/* Zeile(n) hinzufügen
+			 * Zeile(n) wird/ werden nach ausgewählten Zeilen hinzugefügt oder nach der letzten Zeile
+			 */
+			case "addRow":
+				if(rows.length > 0)
+				{
+					for(int i=0; i < rows.length; i++)
+					{
+						model.insertRow(rows[i]+i+1, defaultRowdata);
+					}
+					gui.table.clearSelection();
+					break;
+				} else {
+					model.addRow(defaultRowdata);
+					break;
+				}
+			/* Zeile(n) löschen
+			 * Wenn keine Zeile ausgewählt, erscheint Fehlerfenster.
+			 */
+			case "removeRow":
+				if(rows.length < 0)
+				{
+					gui.errorMessage("Zeile auswählen, die gelöscht werden soll", "Zeile löschen fehlgeschlagen!");
+					break;
+				} 
+				else
+				{
+					for(int i=0; i < rows.length; i++)
+					{
+						model.removeRow(rows[i]-i);
+					}
+					break;
+				}
+		}
+		
+	}
+	
+	public void actionButton(ActionEvent event)
+	{
+		assert config != null;
+		
+		// Ohne GUI können wir nichts machen
+		if(gui == null)
+		{
+			log.error("actionButton(): Keine GUI gesetzt!");
+			return;
+		}
+		
+		switch(event.getActionCommand())
+		{
+			case "save":
+				config.exportSettings(gui, gui);
+				break;
+			case "load":
+				config.importSettings(gui, gui);
+				// Einstellungen neu laden
+				gui.loadSettings();
+				break;
+			case "cancel":
+				close();
+				return;
+			case "apply":
+			case "ok":
+				try
+				{
+					speichereStrecken();
+				}
+				catch(NumberFormatException e)
+				{
+					gui.errorMessage("Fehler beim Speichern der Einstellungen");
+					return;
+				}
+				
+				if(!config.schreibeEinstellungen())
+				{
+					log.error("Fehler beim Speichern der Einstellungen");
+					gui.errorMessage("Fehler beim Speichern der Einstellungen");
+					return;
+				}
+				
+				// OK: Fenster schließen
+				if(event.getActionCommand() == "ok")
+				{
+					close();
+			        return;
+				}
+				break;
+			default:
+				log.error("actionButton: actionCommand nicht erkannt: {}", event.getActionCommand());
+		}
+	}
+		
+	protected void speichereStrecken()
+	{
+		
+	}
+	
+	protected void close()
+	{
+		gui.close();
+        gui = null;
+        onClose.run();
+	}
+	
+	protected int parseIntField(String name, String input)
+	{
+		if(input == null || input.trim().isEmpty())
+		{
+			log.error("{}: Leerer String", name);
+			gui.errorMessage(name+": Dargestellte Zeit: Feld ist leer");
+			throw new NumberFormatException();
+		}
+		try
+		{
+			int hpsInt = Integer.parseInt(input);
+			if(hpsInt < 0)
+			{
+				log.error("{}: Wert kleiner 0", name);
+				gui.errorMessage(name+": Nur positive ganze Zahlen erlaubt.");
+				throw new NumberFormatException();
+			}
+			return hpsInt;
+		}
+		catch(NumberFormatException e)
+		{
+			log.error(name+": NumberformatException", e);
+			gui.errorMessage(name+": Nur positive ganze Zahlen erlaubt.");
+			throw new NumberFormatException();
+		}
+	}
+}
