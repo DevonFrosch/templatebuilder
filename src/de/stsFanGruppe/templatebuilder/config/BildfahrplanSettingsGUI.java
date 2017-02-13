5b000258218e1acd45605716fe606a87730f7b27
DevonFrosch <bitbucket@devonfrosch.eu>
2017-02-13 15:40:31 +0100

    BT090: controller.close() beim Schließen von BildfahrplanSettingsGUI aufrufen

--- src/de/stsFanGruppe/templatebuilder/config/BildfahrplanSettingsGUI.java
+++ src/de/stsFanGruppe/templatebuilder/config/BildfahrplanSettingsGUI.java
@@ -2,6 +2,8 @@ package de.stsFanGruppe.templatebuilder.config;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import com.jgoodies.forms.layout.FormLayout;
@@ -69,7 +71,11 @@ public class BildfahrplanSettingsGUI extends JDialog implements GUI
 		this.config = controller.getConfig();
 		controller.setSettingsGUI(this);
 
-		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+		addWindowListener(new WindowAdapter() { public void windowClosing(WindowEvent e) {
+			controller.close();
+		}});
+		
 		setBounds(100, 100, 450, 500);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
