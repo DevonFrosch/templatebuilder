1d7c849917b4b087dbcc4acd068746b8c04221e1
DevonFrosch <bitbucket@devonfrosch.eu>
2017-02-16 17:05:57 +0100

    BT025: FahrtenFarbeConfig überarbeitet

--- src/de/stsFanGruppe/templatebuilder/config/FahrtenFarbeConfig.java
+++ src/de/stsFanGruppe/templatebuilder/config/FahrtenFarbeConfig.java
@@ -10,15 +10,10 @@ import de.stsFanGruppe.tools.PreferenceHandler;
 public class FahrtenFarbeConfig extends ConfigController
 {
 	
-	private static PreferenceHandler prefs;
+	private PreferenceHandler prefs;
 	private static FahrtenFarbeSettingsGUI gui;
 	
-	// Farbeinstellungen
-	public static final String CONFIG_LINIEN_FARBE = "bildfahrplan/darstellung/linie/farben";
-	public static final String CONFIG_LINIEN_STAERKE = "bildfahrplan/darstellung/linie/staerke";
-	public static final String CONFIG_LINIEN_ART = "bildfahrplan/darstellung/linie/art";
-	
-	// Linienarten
+	// TODO: alt: Linienarten
 	public static final float[] DURCHGEZOGENE_LINIE = {0};
 	public static final float[] GEPUNKTE_LINIE = {10, 10};
 	public static final float[] GESTRICHELTE_LINIE = {30, 10};
@@ -26,94 +21,58 @@ public class FahrtenFarbeConfig extends ConfigController
 	public static final float[] KURZ_KURZ_LANG_LINIE = {10, 10, 10, 10, 30, 10};
 	public static final float[] KURZ_LANG_LANG_LINIE = {10, 10, 30, 10, 30, 10};
 	
-	private static final Color DEFAULT_LINIEN_FARBE = Color.BLACK;
-	private static final int DEFAULT_LINIEN_STAERKE = 1;
-	private static final float[] DEFAULT_LINIEN_ART = DURCHGEZOGENE_LINIE;
+	// Farbeinstellungen
+	public static final String CONFIG_STANDARD_LINIEN_FARBE = "bildfahrplan/darstellung/linie/farben";
+	public static final String CONFIG_STANDARD_LINIEN_STAERKE = "bildfahrplan/darstellung/linie/staerke";
+	public static final String CONFIG_STANDARD_LINIEN_TYP = "bildfahrplan/darstellung/linie/art";
 	
-	// Linienarten
-	public static enum LineType
-	{
-		SOLID_LINE
-		{
-			public Stroke getStroke()
-			{
-				return new BasicStroke(3);
-			}
-		},
-		DOTTED_LINE
-		{
-			public Stroke getStroke()
-			{
-				return new BasicStroke(3, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER, 1.0f, GEPUNKTE_LINIE, 1);
-			}
-		},
-		DASEHED_LINE
-		{
-			public Stroke getStroke()
-			{
-				return new BasicStroke(3, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER, 1.0f, GESTRICHELTE_LINIE, 1);
-			}
-		},
-		SHORT_LONG_LINE
-		{
-			public Stroke getStroke()
-			{
-				return new BasicStroke(3, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER, 1.0f, KURZ_LANG_LINIE, 1);
-			}
-		},
-		SHORT_SHORT_LONG_LINE
-		{
-			public Stroke getStroke()
-			{
-				return new BasicStroke(3, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER, 1.0f, KURZ_KURZ_LANG_LINIE,
-						1);
-			}
-		},
-		SHORT_LONG_LONG_LINE
-		{
-			public Stroke getStroke()
-			{
-				return new BasicStroke(3, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER, 1.0f, KURZ_LANG_LANG_LINIE,
-						1);
-			}
-		};
-		
-		public abstract Stroke getStroke();
-	}
+	public static final Color DEFAULT_STANDARD_LINIEN_FARBE = Color.BLACK;
+	public static final int DEFAULT_STANDARD_LINIEN_STAERKE = 1;
+	public static final LineType DEFAULT_STANDARD_LINIEN_TYP = LineType.SOLID_LINE;
 	
-	public Color getDefaultLinienFarbe()
+	// Konstruktoren
+	public FahrtenFarbeConfig()
 	{
-		return DEFAULT_LINIEN_FARBE;
-	}
-	
-	public static int getDefaultLinienStaerke()
-	{
-		return DEFAULT_LINIEN_STAERKE;
-	}
-	
-	public static String getDefaultLinienStaerkeToString()
-	{
-		StringBuilder sb = new StringBuilder();
-		sb.append(DEFAULT_LINIEN_STAERKE);
-		String linienStaerke = sb.toString();
-		return linienStaerke;
-	}
-	
-	public static float getDefaultLinienArt(int i)
-	{
-		return DEFAULT_LINIEN_ART[i];
-	}
-	
-	public String colorToString(Color color)
-	{
-		if(Color.BLACK.equals(color))
-		{
-			String c = "Schwarz";
-			return c;
-		}
-		return "Schwarz2";
+		log.debug("Neue BildfahrplanConfig()");
+		this.prefs = new PreferenceHandler(FahrtenFarbeConfig.class, () -> notifyChange());
+		assert prefs != null;
 	}
 
+	// Getter / Setter
+	public Color getStandardLinienFarbe()
+	{
+		return prefs.getColor(CONFIG_STANDARD_LINIEN_FARBE, DEFAULT_STANDARD_LINIEN_FARBE);
+	}
+	public void setStandardLinienFarbe(Color standardLinienFarbe)
+	{
+		prefs.setColor("standardLinienFarbe", CONFIG_STANDARD_LINIEN_FARBE, standardLinienFarbe);
+	}
+	public int getStandardLinienStärke()
+	{
+		return prefs.getInt(CONFIG_STANDARD_LINIEN_STAERKE, DEFAULT_STANDARD_LINIEN_STAERKE);
+	}
+	public void setStandardLinienStärke(int standardLinienStärke)
+	{
+		prefs.setInt("standardLinienStärke", CONFIG_STANDARD_LINIEN_STAERKE, standardLinienStärke);
+	}
+	public LineType getStandardLinienTyp()
+	{
+		String name = prefs.getString(CONFIG_STANDARD_LINIEN_TYP, null);
+		return getLineType(name);
+	}
+	public String getStandardLinienTypString()
+	{
+		return prefs.getString(CONFIG_STANDARD_LINIEN_TYP, DEFAULT_STANDARD_LINIEN_TYP.name());
+	}
+	public void setStandardLinienTyp(LineType standardLinienFarbe)
+	{
+		prefs.setString("standardLinienFarbe", CONFIG_STANDARD_LINIEN_TYP, standardLinienFarbe.name());
+	}
+	public void setStandardLinienTyp(String standardLinienFarbe)
+	{
+		prefs.setString("standardLinienFarbe", CONFIG_STANDARD_LINIEN_TYP, standardLinienFarbe);
+	}
+	
 	public boolean schreibeEinstellungen()
 	{
 		return prefs.schreibeEinstellungen();
@@ -125,5 +84,63 @@ public class FahrtenFarbeConfig extends ConfigController
 	public boolean exportXML(OutputStream os)
 	{
 		return prefs.exportXML(os);
+	}
+	
+	// Linienarten
+	public static enum LineType
+	{
+		SOLID_LINE {
+			public Stroke getStroke() {
+				return new BasicStroke(3);
+			}
+		},
+		DOTTED_LINE {
+			public Stroke getStroke() {
+				return new BasicStroke(3, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER, 1.0f, GEPUNKTE_LINIE, 1);
+			}
+		},
+		DASEHED_LINE {
+			public Stroke getStroke() {
+				return new BasicStroke(3, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER, 1.0f, GESTRICHELTE_LINIE, 1);
+			}
+		},
+		SHORT_LONG_LINE {
+			public Stroke getStroke() {
+				return new BasicStroke(3, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER, 1.0f, KURZ_LANG_LINIE, 1);
+			}
+		},
+		SHORT_SHORT_LONG_LINE {
+			public Stroke getStroke() {
+				return new BasicStroke(3, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER, 1.0f, KURZ_KURZ_LANG_LINIE, 1);
+			}
+		},
+		SHORT_LONG_LONG_LINE {
+			public Stroke getStroke() {
+				return new BasicStroke(3, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER, 1.0f, KURZ_LANG_LANG_LINIE, 1);
+			}
+		};
+		public abstract Stroke getStroke();
+	}
+	
+	public static String getLineTypeName(LineType type)
+	{
+		return type.name();
+	}
+	public static LineType getLineType(String name)
+	{
+		if(name == null)
+		{
+			return DEFAULT_STANDARD_LINIEN_TYP;
+		}
+		
+		try
+		{
+			return Enum.valueOf(LineType.class, name);
+		}
+		catch(IllegalArgumentException e)
+		{
+			log.warn("Unknown LineType {}", name);
+			return DEFAULT_STANDARD_LINIEN_TYP;
+		}
 	}
 }
