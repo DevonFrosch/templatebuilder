db02de2d8fa34877cc59b9fc5cd9dce9c5265101
DevonFrosch <git@devonfrosch.eu>
2024-08-18 20:47:46 +0200

    FahrtEditor: Zusätzliche Zeiten am Anfang und Ende anzeigen, um einfacher debuggen zu können

--- src/de/stsFanGruppe/templatebuilder/editor/fahrtEditor/FahrtEditorGUIController.java
+++ src/de/stsFanGruppe/templatebuilder/editor/fahrtEditor/FahrtEditorGUIController.java
@@ -1,13 +1,13 @@
 package de.stsFanGruppe.templatebuilder.editor.fahrtEditor;
 
 import java.awt.event.ActionEvent;
+import java.util.Collection;
 
 import javax.swing.event.TableModelEvent;
 
 import de.stsFanGruppe.templatebuilder.editor.EditorDaten;
 import de.stsFanGruppe.templatebuilder.zug.Fahrplanhalt;
 import de.stsFanGruppe.templatebuilder.zug.Fahrt;
-import de.stsFanGruppe.tools.FirstLastLinkedList;
 import de.stsFanGruppe.tools.NullTester;
 import de.stsFanGruppe.tools.TimeFormater;
 
@@ -68,7 +68,7 @@ public class FahrtEditorGUIController
 		}
 		aktuelleFahrt = fahrt;
 		
-		FirstLastLinkedList<Fahrplanhalt> halte = fahrt.getFahrplanhalte();
+		Collection<Fahrplanhalt> halte = fahrt.getFahrplanhalte();
 		
 		String[][] inhalte = new String[halte.size()][3];
 		
@@ -78,14 +78,18 @@ public class FahrtEditorGUIController
 			String an = TimeFormater.optionalDoubleToString(halt.getAnkunft());
 			String ab = TimeFormater.optionalDoubleToString(halt.getAbfahrt());
 			
-			if(halt == halte.first() && halte.size() >= 2)
+			// TODO: Entscheiden, ob Ankünfte am ersten und Abfahrten am letzten Halt angezeigt werden sollen
+			// Zur Entwicklung ist es sinnvoller, alles anzuzeigen
+			/*
+			if(j == 0 && halte.size() >= 2)
 			{
 				an = "";
 			}
-			if(halt == halte.last() && halte.size() >= 2)
+			if(j == halte.size() - 1 && halte.size() >= 2)
 			{
 				ab = "";
 			}
+			*/
 			
 			inhalte[j][0] = halt.getBetriebsstelle().getName();
 			inhalte[j][1] = an;
@@ -103,7 +107,6 @@ public class FahrtEditorGUIController
 			return;
 		}
 		int column = event.getColumn();
-		FirstLastLinkedList<Fahrplanhalt> halte = aktuelleFahrt.getFahrplanhalte();
 		
 		for(int row = event.getFirstRow(); row <= event.getLastRow(); row++)
 		{
